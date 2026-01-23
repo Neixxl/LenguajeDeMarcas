2f407765fbc66606bf00b8beed8a78b2b98c9951
neixxl <delrioneizan@gmail.com>
2026-01-23 21:13:21 +0100

    scheduled commit

--- Calculadora/js/javascript.js
+++ Calculadora/js/javascript.js
@@ -3,43 +3,135 @@
 const CE = document.getElementById("CE");
 const C = document.getElementById("C");
 const DEL = document.getElementById("DEL");
-const div = document.getElementById("div");
-const mult = document.getElementById("mult");
-const resta = document.getElementById("resta");
-const suma = document.getElementById("suma");
-const uno = document.getElementById("uno");
-const dos = document.getElementById("dos");
-const tres = document.getElementById("tres");
-const cuatro = document.getElementById("cuatro");
-const cinco = document.getElementById("cinco");
-const seis = document.getElementById("seis");
-const siete = document.getElementById("siete");
-const ocho = document.getElementById("ocho");
-const nueve = document.getElementById("nueve");
-const cero = document.getElementById("cero");
 const igual = document.getElementById("igual");
 const punto = document.getElementById("punto");
 const resultado = document.getElementById("resultado");
+const numero = document.querySelectorAll(".numero");
+const operador = document.querySelectorAll(".operator");
 
 // === PANTALLA ===
 const pantalla = document.getElementById("pantalla");
-let text = pantalla.innerText
+let span = pantalla.querySelector("span");
 
-
+// === CALCULOS ===
+let numeros = [];
+let opEstado = 1;
+const operaciones = ["suma", "resta", "divi", "multi"];
+let operadorActual = null;
 
 document.addEventListener("DOMContentLoaded", () => {
 
-if(DEL){DEL.addEventListener("click", () => {pantalla.textContent.charAt(0) = "0";});}
-if(CE){CE.addEventListener("click", () => {pantalla.textContent = "";});}    
-if(uno){uno.addEventListener("click", () => {pantalla.textContent += "1";});}
-if(dos){dos.addEventListener("click", () => {pantalla.textContent += "2";});}
-if(tres){tres.addEventListener("click", () => {pantalla.textContent += "3";});} 
-if(cuatro){cuatro.addEventListener("click", () => {pantalla.textContent += "4";});}
-if(cinco){cinco.addEventListener("click", () => {pantalla.textContent += "5";});}
-if(seis){seis.addEventListener("click", () => {pantalla.textContent += "6";});}
-if(siete){siete.addEventListener("click", () => {pantalla.textContent += "7";});}
-if(ocho){ocho.addEventListener("click", () => {pantalla.textContent += "8";});}
-if(nueve){nueve.addEventListener("click", () => {pantalla.textContent += "9";});}
-if(cero){cero.addEventListener("click", () => {pantalla.textContent += "0";});}
+    if (DEL) {      // === DEL ===
+        DEL.addEventListener("click", () => {
+            numeros[opEstado] = numeros[opEstado].slice(0, -1);
+            pantalla.textContent = numeros[opEstado];
+        });
+    }
+
+    if (CE) {      // === CE ===    
+        CE.addEventListener("click", () => {
+            pantalla.textContent = 0;
+            numeros = [];
+            opEstado = 1;
+        });
+    }
+
+    if (numero) {  // === NUMEROS ===
+        numero.forEach((numero) => {
+            numero.addEventListener("click", () => {
+                if (numeros[opEstado] === undefined) {
+                    numeros[opEstado] = numero.textContent;
+                } else if (numeros[opEstado].length < 12) {
+                    numeros[opEstado] += numero.textContent;
+                }
+                pantalla.textContent = numeros[opEstado];
+                borrarClase();
+
+                console.log(numeros[opEstado]);
+            });
+        });
+    }
+
+    if (operador) {         // === OPERADORES ===
+        operador.forEach((operador) => {
+            operador.addEventListener("click", () => {
+                if (opEstado === 1) {
+                    manejarOperador(operador);
+                }
+            });
+        });
+    }
+
+    if (igual) {       // === IGUAL === 
+        igual.addEventListener("click", () => {
+            if (opEstado >= 2 && numeros[2] !== undefined) {
+                calcularOperacion();
+            }
+        });
+    }
 
 });
+
+function manejarOperador(operador) {
+    if (operador) {
+        pantalla.textContent = 0;
+        opEstado = 2;
+        operadorActual = operador.id;
+        console.log(operador); //DEBUG
+        console.log(opEstado); //DEBUG
+    }
+}
+
+function calcularOperacion() {
+    numeros[2] = parseFloat(numeros[2]);
+    numeros[1] = parseFloat(numeros[1]);
+    borrarClase();
+    switch (operadorActual) {
+        case "suma":
+            numeros[3] = numeros[1] + numeros[2];
+            asignarClase(operadorActual);
+            break;
+        case "resta":
+            numeros[3] = numeros[1] - numeros[2];
+            asignarClase(operadorActual);
+            break;
+        case "divi":
+            if (numeros[2] === 0) {
+                numeros[3] = "ERROR";
+                asignarClase(operadorActual);
+            } else {
+                numeros[3] = numeros[1] / numeros[2];
+                asignarClase(operadorActual);
+            }
+            break;
+        case "multi":
+            numeros[3] = numeros[1] * numeros[2];
+            asignarClase(operadorActual);
+            break;
+    }
+    let resultado = numeros[3].toString();
+    if (resultado.length > 12) {
+        pantalla.textContent = parseFloat(resultado).toPrecision(8);
+    } else {
+        pantalla.textContent = resultado;
+    }
+    console.log("numeros1 " + numeros[1]); //DEBUG
+    console.log("numeros2 " + numeros[2]); //DEBUG
+    console.log("numeros3 " + numeros[3]); //DEBUG
+    numeros = [];
+    opEstado = 1;
+    operadorActual = null;
+}
+
+
+
+function asignarClase(operadorActual) {
+    pantalla.classList.add(`${operadorActual}Text`); // agrega la clase de la operacion actual
+}
+
+function borrarClase() {
+    operaciones.forEach(op => {
+        pantalla.classList.remove(`${op}Text`);     // quita la clase de la operacion anterior
+    });
+}
+
