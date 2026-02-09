aebb2510a84d5e1db7e322656f9e02942f114eed
neixxl <delrioneizan@gmail.com>
2026-02-09 20:38:00 +0100

    scheduled commit

---
+++ Proyecto RA03 LMSGI/js/javascript.js
@@ -0,0 +1,17 @@
+
+
+const opciones = ["piedra", "papel", "tijera", "lagarto", "spock"];
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+})
+
+setTimeout(() => {
+    const contenedor = document.querySelector('main');
+    if (contenedor) {
+        contenedor.style.opacity = '1';
+    }
+}, 100);
